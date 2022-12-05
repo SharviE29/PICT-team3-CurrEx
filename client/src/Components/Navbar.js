d5c452f82b2f4e71fdb1fdcb400d6bd28f539935
BhaktiMahurkar <bhaktiamm@gmail.com>
2022-12-05 21:45:20 +0530

    Frontend update 1

---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">CurrEx</a>
+   
+    
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
