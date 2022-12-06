c916440b98e467858804e6a8fcc728527ee7eb00
Sharvi Endait <sharviendait@gmail.com>
2022-12-06 14:38:28 +0530

    route1 added

--- api/app.js
+++ api/app.js
@@ -1,18 +1,67 @@
-const express=require('express')
-const db=require('./db')
-const cors = require('cors')
+// const express=require('express')
+// const db=require('./db')
+// const cors = require('cors')
 
 
-const app=express()
+// const app=express()
 
-app.use(express.json())
-app.use(cors())
+// app.use(express.json())
+// app.use(cors())
 
-app.get('/ping', (req, res) => {
-    res.send('pong')
+// app.get('/ping', (req, res) => {
+//     res.send('pong')
+// })
+
+
+// app.
+
+// app.listen(9000,()=>{
+//     console.log("Api running on 9000")
+// })
+
+
+const express = require("express");
+const client = require("./db");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/ping", (req, res) => {
+    res.send("pong");
+});
+
+
+app.get('/graph/monthly', async (req,res) => {
+    
 })
 
 
-app.listen(9000,()=>{
-    console.log("Api running on 9000")
-})
+app.get("/curr/", async (req, res) => {
+    let year = 'Year_';
+    year += req.body.year;
+
+
+    console.log(year);
+
+    try {
+        const db = client.db("new_test");
+        const col = db.collection("2012_data");
+        const data = await col.aggregate([
+            {$unwind: "$"+year },
+            {$group: {_id: "$"+year}},
+            {$group: {_id: "$_id.CURR"}}
+        ]).toArray();
+        
+        res.send(data);
+
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.listen(8000, () => {
+    console.log("Api running on 9000");
+});
--- api/db.js
+++ api/db.js
@@ -1,11 +1,38 @@
-const mongoose=require('mongoose')
+// const mongoose=require('mongoose')
 
-const url='mongodb://localhost/currencyexchange'
+// const url='mongodb://localhost/currencyexchange'
 
-const connectToMongo=()=>{
-    mongoose.connect(url,{useNewUrlParser:true},()=>{
-        console.log("connected to mongo!")
-    })
-}
+// const connectToMongo=()=>{
+//     mongoose.connect(url,{useNewUrlParser:true},()=>{
+//         console.log("connected to mongo!")
+//     })
+// }
 
-module.exports=connectToMongo
+// module.exports=connectToMongo
+
+
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb://localhost/";
+
+const dbName = "currencyexchange";
+
+const client = new MongoClient(url);
+
+const connectToMongo = async () => {
+    try {
+        await client.connect();
+        console.log("Connected to mongo!");
+
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+console.log("IN DB.JS");
+
+connectToMongo()
+
+
+module.exports = client;
